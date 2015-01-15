a67cd4df992bff1c08487c671301ff0cb068eae5
Donna Malayeri <DonnaM@Donnas-MacBook-Air.local>
2015-01-15 12:29:44 -0800

    Update iOS offline sample to be similar to new iOS quick start

---
+++ TodoOffline/iOS/WindowsAzureMobileServices.framework/Versions/Current/Headers/WindowsAzureMobileServices.h
@@ -0,0 +1,27 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+#ifndef WindowsAzureMobileServices_WindowsAzureMobileServices_h
+#define WindowsAzureMobileServices_WindowsAzureMobileServices_h
+
+#import "MSClient.h"
+#import "MSTable.h"
+#import "MSQuery.h"
+#import "MSUser.h"
+#import "MSFilter.h"
+#import "MSError.h"
+#import "MSLoginController.h"
+#import "MSTableOperation.h"
+#import "MSSyncContext.h"
+#import "MSSyncTable.h"
+#import "MSTableOperationError.h"
+#import "MSCoreDataStore.h"
+#import "MSPush.h"
+#import "MSDateOffset.h"
+
+#define WindowsAzureMobileServicesSdkMajorVersion 2
+#define WindowsAzureMobileServicesSdkMinorVersion 0
+#define WindowsAzureMobileServicesSdkBuildVersion 30114
+
+#endif
