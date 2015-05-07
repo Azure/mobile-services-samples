1a9401fd6e69cd1ffc6ae68303e79334d346354c
Norm Estabrook <normesta@microsoft.com>
2015-05-07 12:42:52 -0700

    Removing an accidental checkin

---
+++ GettingStartedWithPushXamarinForms_old/ToDoAzure/ToDoAzure/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoAzure
+{
+    public static class Constants
+    {
+        // Replace strings with your mobile services url and key.
+        public static string ApplicationURL = @"https://normstodolist.azure-mobile.net/";
+        public static string ApplicationKey = @"dQPNpchVSpSozaVGHqlUWSsdviYqWB38";
+
+        // Google API Project number.
+        public const string SenderID = "226750010831"; 
+    }
+}
---
+++ GettingStartedWithPushXamarinForms_old/ToDoAzure/ToDoAzure/Models/ToDoItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.ComponentModel;
+
+namespace ToDoAzure
+{
+    public class ToDoItem
+    {
+        string _id;
+        string _name;
+        string _notes;
+        bool _done;
+
+        [JsonProperty(PropertyName = "id")]
+        public string ID
+        {
+            get { return _id; }
+            set { _id = value;}
+        }
+
+        [JsonProperty(PropertyName = "text")]
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value;}
+        }
+
+        [JsonProperty(PropertyName = "notes")]
+        public string Notes
+        {
+            get { return _notes; }
+            set { _notes = value;}
+        }
+
+        [JsonProperty(PropertyName = "complete")]
+        public bool Done
+        {
+            get { return _done; }
+            set { _done = value;}
+        }
+
+        [JsonProperty(PropertyName = "deviceToken")]
+        public string DeviceToken { get; set; }
+
+    }
+}
