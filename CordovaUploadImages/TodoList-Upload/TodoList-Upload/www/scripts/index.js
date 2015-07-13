11b48c4c3d54ad3825644e933967fa38e65533eb
Mikejo5001 <m.jones168@outlook.com>
2015-07-13 09:48:01 -0700

    dev14 rtm updates

--- CordovaUploadImages/TodoList-Upload/TodoList-Upload/www/scripts/index.js
+++ CordovaUploadImages/TodoList-Upload/TodoList-Upload/www/scripts/index.js
@@ -10,8 +10,8 @@
 
         // Initialize the Mobile Services client here.
         var client = new WindowsAzure.MobileServiceClient(
-            'https://MY_MOBILE_SERVICE.azure-mobile.net/',
-            'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+            'https://zumo-mdha-service.azure-mobile.net/',
+            'zlkepWTztMiZAvlyoVXSuJfmkSmhli30');
 
         // Get the existing TodoItem table for storage.
         var todoItemTable = client.getTable('zumoTable');
